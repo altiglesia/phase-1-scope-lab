8051b09ef15aa1bb363d3b12bfbc4270426cd069
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-09-23 15:45:10 -0400

    commit tree 1746175764f6a31c9f1c0f218c84d1572bf07b33

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// const globalVar = 1;
+
+// function firstFunc() {
+//     const firstVar = 2;
+
+//     function secondFun() {
+//         const secondVar = 3;
+//         return secondVar + firstVar + globalVar;
+//     }
+
+//     const resultFromSecondFunc = secondFunc();
+//     return resultFromSecondFunc;
+// }
+
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'person';
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = 'joe'
+}
+
